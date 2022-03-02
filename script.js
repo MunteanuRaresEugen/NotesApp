d400e1889744614ee370c21a0b533a4a7f9a69e1
MunteanuRaresEugen <74292996+MunteanuRaresEugen@users.noreply.github.com>
2022-03-02 19:44:18 +0200

    added delete all button

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 let addBtn = document.getElementById('add-btn');
 let addTitle = document.getElementById('note-title');
 let addText = document.getElementById('note-text');
+let deleteBtn = document.getElementById('delete-all-btn');
 
 addBtn.addEventListener("click", (e) => {
     if (addTitle.value === "" || addText.value === ""){
         return alert("Try write sth on title and details");
     }
+    let notesObj;
     let notes = localStorage.getItem("notes");
     if (notes === null) {
         notesObj = [];
@@ -24,10 +26,26 @@ addBtn.addEventListener("click", (e) => {
     showNotes();
 }) 
 
+deleteBtn.addEventListener("click", (e) => {
+    let notes = localStorage.getItem("notes");
+    let notesObj;
+    if (notes == null) {
+        notesObj = [];
+    } else {
+        notesObj = JSON.parse(notes);
+    }
+    
+    notesObj.splice(0, notesObj.length);
+    localStorage.setItem("notes", JSON.stringify(notesObj));
+
+    showNotes();
+})
+
 // Function to show notes 
 
 const showNotes = () => {
     let notes = localStorage.getItem("notes");
+    let notesObj;
     if (notes === null) {
         notesObj = [];
     } else {
@@ -65,7 +83,7 @@ const showNotes = () => {
 
 const deleteNote = (index) => {
     let confirmDel = confirm("Are you sure you want to delete this note");
-    
+    let notesObj;
     if (confirmDel == true) {
         let notes = localStorage.getItem("notes");
         if (notes == null) {
@@ -84,6 +102,7 @@ const deleteNote = (index) => {
 
 const editNote = (index) => {
     let notes = localStorage.getItem("notes");
+    let notesObj;
     if (addTitle.value !== "" || addText.value !== "") {
         return alert("Please clear the form before editing a notes");
     }
